300efd8f9426c5d5d55857985bdf23c148d105b0
Alberes Pereira <alberesbass@hotmail.com>
2024-08-20 17:49:59 -0300

    init: 🎉 Primeiro commit do projeto

---
+++ index.js
@@ -0,0 +1,9 @@
+let resposta = confirm(
+  "Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?"
+);
+
+if (resposta) {
+  alert("Ótimo! Nós temos as melhores camas de toda a região!");
+} else if (resposta === false) {
+  alert("Que pena! Você parecia ser uma pessoa mais legal");
+}
